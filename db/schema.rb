9f1f46e1554749d379b264ff9eb52bd6cdd68901
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-03-31 16:46:00 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_033520) do
+ActiveRecord::Schema.define(version: 2021_03_31_074333) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
